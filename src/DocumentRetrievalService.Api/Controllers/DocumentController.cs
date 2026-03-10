acd666809bf02d9474017469ca78d1fc0256556a
SpicyCoder <spk2naveen@hotmail.com>
2026-03-10 23:00:58 +0530

    Update project names

---
+++ src/DocumentRetrievalService.Api/Controllers/DocumentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentRetrievalService.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DocumentController : ControllerBase
+{
+    [HttpGet("report")]
+    public async Task<IActionResult> GetReport()
+    {
+        return Ok();
+    }
+}
--- src/DocumentRetrievalService.Api/Program.cs
+++ src/DocumentRetrievalService.Api/Program.cs
@@ -1,23 +1,26 @@
+using AspNetCore.Swagger.Themes;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(
+        Theme.Futuristic,
+        options =>
+        {
+            options.SwaggerEndpoint("/openapi/v1.json", "Document Retrieval Service - V1");
+            options.EnableThemeSwitcher(ThemeSwitcherOptions.All());
+            options.ShowBackToTopButton();
+        });
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
